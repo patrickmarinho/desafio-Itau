987fee1151bf9f769a37488dfe58197b1553e68b
Erick <erick.marinho2003@gmail.com>
2024-08-07 00:53:33 -0300

    feat(repository): add method to fetch transactions for the last 60 seconds

--- src/main/java/com/example/desafioitau/repository/TransactionRepository.java
+++ src/main/java/com/example/desafioitau/repository/TransactionRepository.java
@@ -3,12 +3,27 @@ package com.example.desafioitau.repository;
 import com.example.desafioitau.domain.transaction.Transaction;
 import org.springframework.stereotype.Repository;
 
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class TransactionRepository {
     private final List<Transaction> transactions = new ArrayList<>();
+
+    public List<Transaction> getTransactions(){
+        return transactions;
+    }
+
+    public List<Transaction> getLast60SecondsTransactions(){
+        OffsetDateTime oneMinuteAgo = OffsetDateTime.now().minusSeconds(60);
+        List<Transaction> last60SecondTransactions = getTransactions().stream()
+                .filter(transaction -> transaction.getDateTime().isAfter(oneMinuteAgo) || transaction.getDateTime().equals(oneMinuteAgo))
+                .collect(Collectors.toList());
+
+        return last60SecondTransactions;
+    }
 
     public void addTransaction(Transaction transaction) {
         transactions.add(transaction);
