8057de9e552e830d1c0e247b4e0ecb8c23dc05d5
Erick <erick.marinho2003@gmail.com>
2024-08-10 19:39:52 -0300

    feat: add new logging in service layer

--- src/main/java/com/example/desafioitau/controller/TransactionController.java
+++ src/main/java/com/example/desafioitau/controller/TransactionController.java
@@ -2,26 +2,33 @@ package com.example.desafioitau.controller;
 
 import com.example.desafioitau.domain.transaction.Transaction;
 import com.example.desafioitau.service.TransactionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/transaction", consumes = "application/json", produces = "application/json")
+@RequestMapping(value = "/transaction", produces = "application/json")
 public class TransactionController {
+    private static final Logger log = LoggerFactory.getLogger(TransactionController.class);
     @Autowired
     TransactionService transactionService;
 
-    @PostMapping()
+    @PostMapping(consumes = "application/json")
     public ResponseEntity<Void> newTransaction(@RequestBody Transaction transaction){
+        log.info("Received new Transaction request.");
         transactionService.newTransaction(transaction);
+        log.info("Transaction successfully processed.");
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping()
     public ResponseEntity<Void> deleteTransaction(){
+        log.info("Cleaning all transactions.");
         transactionService.deleteTransaction();
+        log.info("All transactions have been cleaned.");
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
--- src/main/java/com/example/desafioitau/service/StatisticsService.java
+++ src/main/java/com/example/desafioitau/service/StatisticsService.java
@@ -3,6 +3,8 @@ package com.example.desafioitau.service;
 import com.example.desafioitau.domain.statistics.Statistics;
 import com.example.desafioitau.domain.transaction.Transaction;
 import com.example.desafioitau.repository.TransactionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,7 @@ import java.util.stream.Collectors;
 
 @Service
 public class StatisticsService {
+    private static final Logger log = LoggerFactory.getLogger(StatisticsService.class);
     @Autowired
     private TransactionRepository transactionRepository;
 
@@ -18,13 +21,14 @@ public class StatisticsService {
 
     //Get Statistic
     public Statistics getStatistics(){
-
+        log.info("Starting statistics calculation for the last 60 seconds of transactions.");
         if(transactionRepository.getLast60SecondsTransactions().isEmpty()){
             statistics.setCount((float) 0);
             statistics.setSum((float) 0);
             statistics.setAvg((float) 0);
             statistics.setMin((float) 0);
             statistics.setMax((float) 0);
+            log.info("No transactions found in the last 60 seconds. Statistics set to zero.");
         }else{
             DoubleSummaryStatistics doubleSummaryStatistics = transactionRepository.getLast60SecondsTransactions().stream()
                     .collect(Collectors.summarizingDouble(Transaction::getAmount));
